065881e206a5c2b5f251ceefda37d059873c9d49
MarioTX <chentianxing2002@gmail.com>
2024-09-05 18:30:08 +0800

    update

--- policy/3D-Diffusion-Policy/train.sh
+++ policy/3D-Diffusion-Policy/train.sh
@@ -1,1 +1,7 @@
-bash scripts/train_policy.sh robot_dp3 pick_cuboid_cylinder_50 pick_cuboid_cylinder_50 0 2
+# bash train.sh shoe_place 20 0
+task_name=${1}
+expert_data_num=${2}
+seed=0
+gpu_id=${3}
+
+bash scripts/train_policy.sh robot_dp3 ${task_name}_${expert_data_num} train ${seed} ${gpu_id}
