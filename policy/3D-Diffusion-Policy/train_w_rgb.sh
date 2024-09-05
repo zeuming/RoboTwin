220ae23c61fee0dfa30ef284625a45e40d71d6c2
TianxingChen <chentianxing2002@gmail.com>
2024-09-05 20:53:18 +0800

    add train_w_rgb

---
+++ policy/3D-Diffusion-Policy/train_w_rgb.sh
@@ -0,0 +1,7 @@
+# bash train.sh shoe_place 20 0
+task_name=${1}
+expert_data_num=${2}
+seed=0
+gpu_id=${3}
+
+bash scripts/train_policy.sh robot_dp3_w_rgb ${task_name}_${expert_data_num} train ${seed} ${gpu_id}
